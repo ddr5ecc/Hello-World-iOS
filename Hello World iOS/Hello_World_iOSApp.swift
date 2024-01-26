a7d2cad4b70efac52f526c602ee6e0a744950d93
ddr5ecc <ddr5ecc@protonmail.ch>
2024-01-25 20:44:50 -0800

    Initial Commit

---
+++ Hello World iOS/Hello_World_iOSApp.swift
@@ -0,0 +1,18 @@
+//
+// Hello_World_iOSApp.swift
+// Hello World iOS
+
+// Created by ddr5ecc.eth on 1/25/24. 
+
+
+
+import SwiftUI
+
+@main
+struct Hello_World_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
