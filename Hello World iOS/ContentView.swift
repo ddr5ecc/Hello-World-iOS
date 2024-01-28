7c45890603057383184e555ed0f9c04a0eee9c56
ddr5ecc <ddr5ecc@protonmail.ch>
2024-01-27 21:10:05 -0800

    20240127

--- Hello World iOS/ContentView.swift
+++ Hello World iOS/ContentView.swift
@@ -5,18 +5,46 @@
 // Created by ddr5ecc.eth on 1/25/24. 
 
 
-
+import AVFoundation
 import SwiftUI
 
 struct ContentView: View {
+    
+    func speak(text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+    }
+    
     var body: some View {
         VStack {
+            
+            Spacer()
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world!").fontWeight(.heavy)
+                .font(.system(.title, design: .rounded))
+            
+            Spacer()
+            
+            Button {
+                speak(text: "Hello")
+                
+            } label: {
+                Text("Hello Button")
+                    .fontWeight(.bold)
+                    .font(.system(.title)) // 會加大Button的大小
+            }
+            .padding()
+            .foregroundColor(Color.green)
+            .background(Color.gray)
+            .cornerRadius(30)
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
